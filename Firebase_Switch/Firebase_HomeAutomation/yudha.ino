cec99f77497bc16c3bf17ed971449ed900a925a4
Call Vin <kanggara75@users.noreply.github.com>
2021-06-25 15:39:40 +0700

    Update yudha.ino
    revisi 3

--- Firebase_Switch/Firebase_HomeAutomation/yudha.ino
+++ Firebase_Switch/Firebase_HomeAutomation/yudha.ino
@@ -1,62 +1,110 @@
-#include <ESP8266WiFi.h>     //Memasukan Library Untuk ESP8266
-#include <FirebaseArduino.h> //Memasukan Lib Firebase (Basis Arduino)
-#define FIREBASE_HOST "tugas-akhir-7d195-default-rtdb.firebaseio.com" //url Firebase
-#define FIREBASE_AUTH "WclzvEO0Yyak65pPrrb4NG12Utt8t38CPeEOBZ9J"  //Token Firebase
-#define WIFI_SSID “KAnggara75"  //SSID Wifi
-#define WIFI_PASSWORD "klapankali2”  //Password wifi
 
+/**
+ * Created by K. Suwatchai (Mobizt)
+ * 
+ * Email: k_suwatchai@hotmail.com
+ * 
+ * Github: https://github.com/mobizt
+ * 
+ * Copyright (c) 2021 mobizt
+ *
+*/
 
-#define Relay1 2 //Pin 16 NodeMcu sebgai D0 PinOut
-int val1;
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "KAnggara75"
+#define WIFI_PASSWORD "klapankali2"
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyD7JczIieKxUBC3LP9lfs1UMU6My2oCJYQ"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "tugas-akhir-7d195-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "admin@tandon.com"
+#define USER_PASSWORD "Lolipop."
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+int count = 0;
 
 void setup()
 {
-  resetFunc();
-  Serial.begin(115200); //Baud Rate untuk Serial Monitor
-  pinMode(Relay1, OUTPUT);
+
+  Serial.begin(115200);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print("=");
-    delay(500);
+    Serial.print(".");
+    delay(300);
   }
   Serial.println();
-  Serial.print("connected:");
+  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
+  Serial.println();
 
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-}
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-void(* resetFunc) (void) = 0;
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
 
-void firebasereconnect()
-{
-  Serial.println("Trying to reconnect"); //menghubungkan kembali jika perangkat gagal terhubung ke firebase
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  resetFunc();
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  //Or use legacy authenticate method
+  //config.database_url = DATABASE_URL;
+  //config.signer.tokens.legacy_token = "<database secret>";
+
+  Firebase.begin(&config, &auth);
+
+  Firebase.reconnectWiFi(true);
 }
 
 void loop()
 {
-  if (Firebase.failed())
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
   {
-    Serial.print("setting number failed:");
-    Serial.println(Firebase.error());
-    firebasereconnect(); //menjalankan fungsi firebasereconnect()
-    return;
-  }
+    sendDataPrevMillis = millis();
 
-  val1 = Firebase.getString("DB/Filter").toInt(); //membaca nilai yang ada pada L1
-  if (val1 == 0)
-  {
-    digitalWrite(Relay1, LOW);
-    Serial.println("light 1 ON");
-  }
-  else if (val1 == 1)
-  {
-    digitalWrite(Relay1, HIGH);
-    Serial.println("light 1 OFF");
+    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/test/int", count) ? "ok" : fbdo.errorReason().c_str());
+
+    Serial.printf("Get int... %s\n", Firebase.RTDB.getInt(&fbdo, "/test/int") ? String(fbdo.intData()).c_str() : fbdo.errorReason().c_str());
+
+    FirebaseJson json;
+    json.add("value", count);
+
+    Serial.printf("Push json... %s\n", Firebase.RTDB.pushJSON(&fbdo, "/test/push", &json) ? "ok" : fbdo.errorReason().c_str());
+
+    json.set("value", count + 100);
+    Serial.printf("Update json... %s\n\n", Firebase.RTDB.updateNode(&fbdo, String("/test/push/" + fbdo.pushName()).c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+
+    count++;
   }
 }
